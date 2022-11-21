5ccb412945da25bb3a56277dc6f0978d96be575b
ohyeah388 <86931546+ohyeah388@users.noreply.github.com>
2022-11-22 05:47:00 +0700

    junko stuff + mami icon

---
+++ assets/preload/stages/Moon.lua
@@ -0,0 +1,98 @@
+function onCreate()
+
+
+	makeLuaSprite('Moon','Moon/1', -900, -500);
+    addLuaSprite('Moon',false);
+    setScrollFactor('Moon', 0.2, 0.9);
+	
+	makeLuaSprite('Moon2','Moon/2', -1200, -950);
+    addLuaSprite('Moon2',false);
+    setScrollFactor('Moon2', 1, 1);
+
+ scaleObject('Moon', 0.85, 0.85);
+ scaleObject('Moon2', 1, 1);
+end
+
+local xx = 500;
+local yy = 330;
+local xx2 = 800;
+local yy2 = 330;
+local ofs = 45;
+local followchars = true;
+local del = 0;
+local del2 = 0;
+local i = 0
+
+function onUpdate(elapsed)
+
+	if getProperty("health") <= 0 then
+		playSound("death",0.7);
+		end
+	if del > 0 then
+		del = del - 1
+	end
+	if del2 > 0 then
+		del2 = del2 - 1
+	end
+    if followchars == true then
+        if mustHitSection == false then
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT-alt' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT-alt' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+        else
+
+            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
+            end
+	    if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+        end
+    else
+        triggerEvent('Camera Follow Pos','','')
+    end
+    
+end
+
+function opponentNoteHit()
+    health = getProperty('health')
+    if getProperty('health') > 0.4 and difficulty == 3 then
+        setProperty('health', health- 0.013);
+    end
+end
