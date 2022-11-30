0fe60e2862cb8c4c86eec6fb97d0579cdbf1b11a
ohyeah388 <86931546+ohyeah388@users.noreply.github.com>
2022-11-30 17:43:17 +0700

    yuyu quote event

--- example_mods/custom_events/ShowImage.lua
+++ example_mods/custom_events/ShowImage.lua
@@ -1,8 +1,8 @@
 function onEvent(name, value1, value2)
 --value1 is image name, value2 is to show (1) or delete (0)
 if name == "ShowImage" and value2 == '1' then
-		makeLuaSprite(value1, value1, 0, 0);
-		scaleObject(value1, 0.68, 0.68);
+		makeLuaSprite(value1, value1, -10, -10);
+		scaleObject(value1, 0.7, 0.7);
 		addLuaSprite(value1, true);
 		cameraShake('other', 0.0004, 4.5);
 		cameraFlash('other','ffffff', 1.6, true);
